d9de9a376c4a95f948d1202eff0145b73873d884
ashrith <ashrith.mudundi@gmail.com>
2024-10-10 12:17:48 -0400

    implemented treeset

---
+++ treeset.java
@@ -0,0 +1,16 @@
+import com.sun.source.tree.Tree;
+
+import java.util.TreeSet;
+
+public class treeset{
+    public static void main(String[] args){
+        TreeSet<Integer> ts = new TreeSet<>();
+
+        ts.add(20);
+        ts.add(15);
+        ts.add(5);
+        ts.add(10);
+
+        System.out.println(ts);
+    }
+}
